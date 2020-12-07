44b9f38ec90c1b6f07025e6ee23e0b99e2ffd9bd
medini wassim <medini.wassim@gmail.com>
2020-12-07 15:20:49 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@
 import './App.css';
 import Profile from './profile/Profile'
 import photo from './profile/profilephoto.png'
+import Welcome from './Welcome'
+
+
+  
+
 
 function App() {
+  const handleName=(name)=> alert(`bonjour ${name}!!`);
   return (
     <div className="App">
       
-      <Profile ><img src={photo} alt='imagefortest' width="200px" style={{borderRadius:'200px'}}></img> </Profile>
-      <Profile ><img src={photo} alt='imagefortest' width="200px" style={{borderRadius:'200px'}}></img> </Profile>
+      <Profile fullName='ali' bio='ali bio' profession='ali profession' handleNameFn={handleName}>
+        <img src={photo} alt='imagefortest' width="200px" style={{borderRadius:'200px'}}></img> 
+     
+          </Profile>
+      <Profile handleNameFn={handleName} ><img src={photo} alt='imagefortest' width="200px" style={{borderRadius:'200px'}}></img> </Profile>
+
+      {/* <Welcome /> */}
     </div>
   );
 }
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types';
 
 
 
-const Profile =(props,{fullName='test',bio='test',profession='test',handleName})=> {
-    handleName=()=>{
-        alert(`bonjour ${fullName}!!`)
-    }
+const Profile =(props)=> {
    
     return (
-
+       
     
 <div >
+{props.handleNameFn(props.fullName)}
+  
    <div style={{backgroundColor:'Grey',width:'250px',margin:'auto'}}>
 
       {props.children}
     
-    <h2 style={{fontFamily:'monospace'}}>Name :  {fullName} </h2>
-    <h2 style={{fontFamily:'monospace'}}> Bio :{bio}</h2>
-    <h2 style={{fontFamily:'monospace'}}>Profession : {profession}</h2>
-   {/* <button onClick={handleName}>show name</button>  */}
-   {handleName()}
+    <h2 style={{fontFamily:'monospace'}}>Name :  {props.fullName} </h2>
+    <h2 style={{fontFamily:'monospace'}}> Bio :{props.bio}</h2>
+    <h2 style={{fontFamily:'monospace'}}>Profession : {props.profession}</h2>
+
+   {/* <button onClick={()=>props.handleNameFn(props.fullName)}>show name</button>  */}
+
  
    </div>
 </div>
@@ -29,11 +29,18 @@ const Profile =(props,{fullName='test',bio='test',profession='test',handleName})
    
 }
 
+Profile.defaultProps={
+fullName:'test' ,
+bio:'test',
+profession:'test'
+
+}
+
 Profile.propTypes={
-fullName:PropTypes.string,
+fullName:PropTypes.string.isRequired,
 bio : PropTypes.string,
 profession : PropTypes.string,
-handleName : PropTypes.func
+handleNameFn : PropTypes.func
 
 };
 
